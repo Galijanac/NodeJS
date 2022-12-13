433e2fc356712eb5aa89c4772e50a078ad8cdc63
nikolaet@gmail.com <etinski.simona@gmail.com>
2022-12-13 22:27:35 +0100

    Error handeling

--- NodeJS-Developer-Cours/WeatherApp/app.js
+++ NodeJS-Developer-Cours/WeatherApp/app.js
@@ -3,16 +3,28 @@ const request = require('postman-request')
 const urlGeoCode = 'http://api.positionstack.com/v1/forward?access_key=cfa8e3d12a7a77662fb7297958a91c07&query=Los%20Angeles&limit=1'
 
 request({url : urlGeoCode, json: true}, (error, response, body) => {
-    latitude = body.data[0].latitude
-    longitude = body.data[0].longitude
-    console.log(latitude + " " + longitude)
-    
-    urlWeather = 'http://api.weatherstack.com/current?access_key=b939b7bec9e0f7d402de55c5af3c95ed&units=f&query=' + latitude + "," + longitude
+    if (error){
+        console.log("Unable to connect ot weather service!")
+    } else if (response.body.error) {
+        console.log("Unable to find location!")
+    } else {
+        latitude = body.data[0].latitude
+        longitude = body.data[0].longitude
+        console.log(latitude + " " + longitude)
+        
+        urlWeather = 'http://api.weatherstack.com/current?access_key=b939b7bec9e0f7d402de55c5af3c95ed&units=f&query=' + latitude + "," + longitude
 
-    request({url : urlWeather, json: true}, (error, response, body) => {
-        console.log(body.current.weather_descriptions[0])
-        console.log("It is currently " + body.current.temperature + " degress out, It feels like " + body.current.feelslike + " degress out.")
-    })
+        request({url : urlWeather, json: true}, (error, response, body) => {
+            if (error) {
+                console.log("Unable to connect ot weather service!")
+            } else if (response.body.error){
+                console.log("Unable to find location!")
+            } else {
+                console.log(body.current.weather_descriptions[0])
+                console.log("It is currently " + body.current.temperature + " degress out, It feels like " + body.current.feelslike + " degress out.")
+            }
+        })
+    }
 })
 
 
